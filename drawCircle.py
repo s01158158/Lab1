70a42c811ae383c44bca768d71c4e90b80437fc4
s01158158 <39176224+s01158158@users.noreply.github.com>
2019-09-08 13:58:26 -0400

    Create drawCircle.py

---
+++ drawCircle.py
@@ -0,0 +1,10 @@
+from dp import *
+>>> import turtle
+>>> t = turtle.Turtle()
+>>> t.up()
+>>> t.backward(200)
+>>> t.left(90)
+>>> t.down()
+>>>drawPolygon(t, 10, 100)
+>>>drawPolygon(t, 3, 200)
+>>>drawPolygon(t, 20, 20)
